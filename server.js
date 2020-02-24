079a08c029eb16f270d0e82b5e8676b3c7f592d2
lucassantosf <lucas46em@gmail.com>
2020-02-24 12:29:43 -0300

    Simples API Restfull em NodeJs

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
 
+//Iniciando o App
 const app = express();
+app.use(express.json());//permitir receber dados via JSON
+app.use(cors());//Liberar a aplicação para ser acessada livremente
 
-app.get('/', (req,res)=>{
-	res.send('Hello World!');
-});
+//Iniciando o DB
+mongoose.connect(
+	'mongodb+srv://lucas:ferreira24680@cluster0-pv8jh.mongodb.net/test?retryWrites=true&w=majority',
+	{ useNewUrlParser: true}
+);
+
+requireDir('./src/models');
+
+//Centralizar arquivo de rotas
+app.use('/api',require('./src/routes'));
 
 app.listen(3001);
