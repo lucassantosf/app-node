d91fe36d85b6daddd001c8ba621257194f9aa6b2
lucassantosf <lucas46em@gmail.com>
2020-02-26 21:08:08 -0300

    Finalizando paginacao, e detalhe do item - React App

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-
+import Routes from './routes';
 import './styles.css';
 
 import Header from './components/Header';
@@ -8,7 +8,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App"> 
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
  
